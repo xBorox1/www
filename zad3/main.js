bfab4eea276f6fd5b6f19955f8f1a056c6b8ba76
Michał Borowski <m.borowski282@gmail.com>
2020-04-13 17:27:10 +0200

    Krok 1

---
+++ zad3/main.js
@@ -0,0 +1,16 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+function zaloguj() {
+    var komunikaty = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        komunikaty[_i] = arguments[_i];
+    }
+    console.log.apply(console, __spreadArrays(["Ależ skomplikowany program!"], komunikaty));
+}
+zaloguj("Ja", "cię", "nie", "mogę");
+//# sourceMappingURL=main.js.map
---
+++ zad3/main.ts
@@ -0,0 +1,5 @@
+function zaloguj(...komunikaty: string[]) {
+	console.log("Ależ skomplikowany program!", ...komunikaty);
+}
+
+zaloguj("Ja", "cię", "nie", "mogę");
